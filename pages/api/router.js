c30db26c04f58d90f1878aa52f45a07b51231eec
Kedasha Kerr <kedashakerr@Kedashas-MacBook-Pro.local>
2023-08-03 16:04:17 -0500

    cors

--- pages/api/router.js
+++ pages/api/router.js
@@ -2,14 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { generateInfo } = require('./generateInfo');
 
-// CORS preflight request handling
-router.options('/generateInfo', (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type")
-    res.setHeader("Access-Control-Allow-Methods", "POST")
-   res.sendStatus(200)
-})
-
 router.post('/generateInfo', generateInfo);
 
 module.exports = router;
